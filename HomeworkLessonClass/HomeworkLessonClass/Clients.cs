b5ab8966891c03f5d4de29d4c39db59b1164a6c3
Maksim Loginov <maksim08@list.ru>
2021-07-06 19:00:48 +0300

    added solution

---
+++ HomeworkLessonClass/HomeworkLessonClass/Clients.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkLessonClass
+{
+    public class Clients : IWorkOperations,IDelivery
+    {
+        public Clients(string firstName, string companyName)
+        {
+            FirstName = firstName;
+            CompanyName = companyName;
+        }
+        public string FirstName { get; set; }
+        public string CompanyName { get; set; }
+
+        //public void MoveToWork()
+        //{
+        //    Console.WriteLine($"Клиент {FirstName} {CompanyName} пришел на работу");
+        //}
+        public void ClosingOfDeal()
+        {
+            Console.WriteLine($"Клиент {FirstName} {CompanyName} купил запчасти");
+        }
+        public void Delivery()
+        {
+            Console.WriteLine($"{FirstName} {CompanyName} Забрал запчасти с транспортной компании");
+        }
+    }
+}
---
+++ HomeworkLessonClass/HomeworkLessonClass/Work.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkLessonClass
+{
+    public class Work : IWorkOperations, IDelivery
+    {
+        public Work( bool sale)
+        {
+            Sale = sale;
+        }
+
+        public const string Name = "Я";
+        public static bool Sale { get; set; }
+
+        public void MoveToWork()
+        {
+            Console.WriteLine("Я пришел на работу");
+        }
+
+        public void CustomerSearch()
+        {
+            Console.WriteLine($"{Name} обзваниваю клиентов и ищу покупателей");
+        }
+        
+        public void Invoicing()
+        {
+            Console.WriteLine($"{Name} Выставляю счёт на необходимые запчасти");
+        }
+
+        public void ClosingOfDeal()
+        {
+            Console.WriteLine("Я сделал продажу");           
+        }
+        public void Delivery()
+        {
+            Console.WriteLine("Отправил запчасти транспортной компанией");
+        }
+    }
+}
