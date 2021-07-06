6a950074dabcb02969973894eb88336a30736f6d
Maksim Loginov <maksim08@list.ru>
2021-07-06 19:04:03 +0300

    add program

---
+++ HomeworkLessonClass/HomeworkLessonClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeworkLessonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Work work = new Work(true);
+            Clients clients = new Clients("Иван", "АгроСлобода");
+            work.MoveToWork();
+            clients.MoveToWork();
+            work.CustomerSearch();
+            work.Invoicing();
+
+            if (Work.Sale == true)
+            {
+                clients.ClosingOfDeal();
+                work.ClosingOfDeal();
+                work.Delivery();
+                clients.Delivery();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
